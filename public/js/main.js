2cbe9487e8c393889ba49530ea1023c5558c8d7a
Gil Solis <gilsolisuw@gmail.com>
2021-03-31 20:38:57 -0700

    completed functionality added

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,38 @@
 const deleteTasks = document.querySelectorAll('.delete')
+const completeTasks = document.querySelectorAll('.complete')
+const removeCompleted = document.querySelectorAll('.removeCompleted')
+
+Array.from(removeCompleted).forEach((element) => {
+    element.addEventListener('click', removeCompletedTask)
+})
+
+Array.from(completeTasks).forEach((element) => {
+    element.addEventListener('click', completeTask)
+})
 
 Array.from(deleteTasks).forEach((element) => {
     console.log(deleteTasks)
     element.addEventListener('click', deleteTask)
 })
+
+async function removeCompletedTask() {
+    const removeTask = this.parentNode.childNodes[1].innerText.trim()
+    try {
+        const response = await fetch('removeCompletedtask', {
+            method: 'delete',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                'completedTask': removeTask
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }
+    catch (err) {
+        console.error(err)
+    }
+}
 
 async function deleteTask() {
     console.log("i've been clicked")
@@ -24,4 +53,38 @@ async function deleteTask() {
     catch (err) {
         console.error(err)
     }
+
+}
+
+async function completeTask() {
+    const taskName = this.parentNode.childNodes[1].innerText.trim()
+    console.log(taskName)
+    try {
+        const response = await fetch('deleteTask', {
+            method: 'delete',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                'taskToDo': taskName
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        // location.reload()
+    }
+    catch (err) {
+        console.error(err)
+    }
+    finally {
+        const response = await fetch('addCompleted', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                'completedTask': taskName
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }
+
 }
--- server.js
+++ server.js
@@ -19,17 +19,15 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-
-// app.get('/', (request, response) => {
-//     let data;
-//     response.render('index.ejs', { info: data })
-// })
-
-app.get('/', (request, response) => {
-    db.collection('toDo').find().toArray()
-        .then(data => {
-            response.render('index.ejs', { info: data })
-        }).catch(err => console.error(err))
+app.get('/', async (request, response) => {
+    let infoData = await db.collection('toDo').find().toArray()
+    let completedData = await db.collection('completed').find().toArray()
+    console.log(infoData, completedData)
+    try {
+        response.render('index.ejs', { info: infoData, completed: completedData })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.post('/addTask', (request, response) => {
@@ -44,7 +42,7 @@ app.post('/addCompleted', (request, response) => {
     db.collection('completed').insertOne(request.body)
         .then(result => {
             console.log('completed task added')
-            response.redirect('/')
+            response.json('task moved to completed')
         })
 })
 
@@ -52,6 +50,14 @@ app.delete('/deleteTask', (request, response) => {
     console.log(request.body)
     console.log("this is req.body.task " + request.body.taskToDo)
     db.collection('toDo').deleteOne({ taskToDo: request.body.taskToDo })
+        .then(result => {
+            console.log('task deleted')
+            response.json('task deleted')
+        })
+        .catch(error => console.error(error))
+})
+app.delete('/removeCompletedtask', (request, response) => {
+    db.collection('completed').deleteOne({ completedTask: request.body.completedTask })
         .then(result => {
             console.log('task deleted')
             response.json('task deleted')
